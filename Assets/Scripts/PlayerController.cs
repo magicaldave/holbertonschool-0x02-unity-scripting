39e417ce500f6762cc40e1f00366599d95d7eb30
magicaldave <smokydave@icloud.com>
2023-02-15 10:01:09 -0600

    Edit: adds new score variable and OnTriggerEnter function to handle coin pickups.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : MonoBehaviour
 	    public Rigidbody m_Rigidbody;
 	    // This vector is used to store the player's actual velocity
 	    private Vector3 velocity = Vector3.zero;
+	    private int score = 0;
 
 	    // Update is called once per frame
 	    void Update()
@@ -17,8 +18,9 @@ public class PlayerController : MonoBehaviour
 		    float horiz = Input.GetAxis("Horizontal");
 		    // This is how I check if you're actually moving.
 		    // The transform information is duplicated, with positive or negative values of 1 representing the translation direction
-		    // When magnitude is greater than zero, you pressed a button. 
+		    // When magnitude is greater than zero, you pressed a button.
 		    Vector3 direction = new Vector3(horiz, 0, vert);
+
 
 		    if (direction.magnitude > 0)
 		    {
@@ -37,5 +39,23 @@ public class PlayerController : MonoBehaviour
 			    }
 		    }
 		    transform.position += velocity * Time.deltaTime;
+
+		    if (score == 40)
+		    {
+			    Debug.Log("All Coins Collected!");
+			    score = 0;
+		    }
+
+    }
+	    void OnTriggerEnter(Collider other)
+	    {
+		    // Increment score when Player touches object with Pickup tag
+		    // Destroy Pickup on contact
+		    if (other.gameObject.CompareTag("Pickup"))
+		    {
+			    Destroy(other.gameObject);
+			    Debug.Log("Score: " + score);
+			    score += 1;
+		    }
 	    }
 }
